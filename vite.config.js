abef1ec4c401b8c77ced4bb47e9f2e7841250a8f
Vivien Wallbaum <viviwbaum@gmail.com>
2025-08-20 09:15:24 +0200

    Vorbereitung zur Veröffentlichung

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Sales-Dashboard/',
   plugins: [
     react(),
     tailwindcss(), 
